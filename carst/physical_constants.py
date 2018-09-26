98d9c1b1126f9a715495be0149a9685e257390a0
Jon Hill <jon.hill@york.ac.uk>
2018-09-26 11:08:28 +1000

    Stripping out 3d stuff

--- carst/physical_constants.py
+++ carst/physical_constants.py
@@ -10,7 +10,6 @@ from .firedrake import Constant
 physical_constants = \
     {'g_grav': Constant(9.81),      # gravitational acceleration
      'rho0': Constant(1000.0),      # reference water density
-     'z0_friction': Constant(0.0),  # bot roughness length for 3D model
      'von_karman': Constant(0.4),   # von Karman constant for bottom log layer
      }
 
