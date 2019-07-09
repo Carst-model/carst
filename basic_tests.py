d6eddbf6ff8462ee366b0d9bc442cfa2d6b3d83c
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2019-07-09 15:07:11 +0100

    Reorganise basic_tests.py for readability

--- basic_tests.py
+++ basic_tests.py
@@ -11,13 +11,30 @@ OUTPUT_TIME = 500
 TIME_STEP = 50
 OUTPUT_FOLDER = "output"
 
+
+# Example land function generator
+def EXAMPLE_LAND(coordinate_space, function_space):
+    return fd.project(100 * fd.tanh(0.0005 * (coordinate_space[0] - 6000)),
+                      function_space,
+                      name="starting_topo")
+
+
+# Example initial condition
+def EXAMPLE_INITIAL_COND(coordinate_space, function_space):
+    fd.project(
+        (20000 * (1 / (2 * fd.sqrt(2 * math.pi * 250 * 250))) * fd.exp(-(
+            (coordinate_space[0] - 6000) *
+            (coordinate_space[0] - 6000)) / (2 * 250 * 250))) +
+        (50000 * (1 / (2 * fd.sqrt(2 * math.pi * 1000 * 1000))) * fd.exp(-(
+            (coordinate_space[0] - 4000) *
+            (coordinate_space[0] - 4000)) / (2 * 1000 * 1000))),
+        function_space)
+
+
 # Initialise a solver and add land
 my_options = CarstOptions(
     fd.RectangleMesh(50, 25, 10000, 5000),
-    lambda coord_space, function_space: fd.project(100 * fd.tanh(0.0005 * (
-        coord_space[0] - 6000)),
-                                                   function_space,
-                                                   name="starting_topo"),
+    EXAMPLE_LAND,
     fd.Constant(25 * fd.sin(START_TIME / 100000 / 180 * math.pi)),
     (
         START_TIME,
@@ -35,16 +52,9 @@ my_solver_real_scale = CarstModel(my_options)
 
 # Run with a sample initial condition
 my_solver_real_scale.set_condition(
-    fd.project(
-        (20000 *
-         (1 /
-          (2 * fd.sqrt(2 * math.pi * 250 * 250))) * fd.exp(-(
-              (my_solver_real_scale.coordinate_space[0] - 6000) *
-              (my_solver_real_scale.coordinate_space[0] - 6000)) /
-                                                           (2 * 250 * 250))) +
-        (50000 * (1 / (2 * fd.sqrt(2 * math.pi * 1000 * 1000))) *
-         fd.exp(-((my_solver_real_scale.coordinate_space[0] - 4000) *
-                  (my_solver_real_scale.coordinate_space[0] - 4000)) /
-                (2 * 1000 * 1000))), my_solver_real_scale.function_space))
+    EXAMPLE_INITIAL_COND(my_solver_real_scale.coordinate_space,
+                         my_solver_real_scale.function_space))
+
+# Iterate
 while my_solver_real_scale.times["current_time"] < 20000:
     my_solver_real_scale.advance()
