6bdc30e17cbe6425825d7f4e05e87b6f7dc2b2dc
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2019-07-19 12:39:20 +0100

    Start to add diamond importing to options.py

--- carst/options.py
+++ carst/options.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import math
 from collections import UserDict
 from enum import Enum
+from itertools import chain
 from typing import Callable, Tuple
-from xml import etree
+from xml.etree import ElementTree
 
 import firedrake as fd
 
@@ -16,63 +18,100 @@ class initialisation_method(Enum):
 
 
 class CarstOptions(UserDict):
+    _STRING_LIT_REPLACEMENTS = (
+        ("tanh", "fd.tanh"),
+        ("sqrt", "fd.sqrt"),
+        ("exp", "fd.exp"),
+        ("x", "self['coordinate_space'][0]"),
+        ("pi", "math.pi"),
+    )
+
     # Dispatch
     def __init__(
             self,
             ini_type: initialisation_method = initialisation_method.raw_values,
             *args,
             **kw_args):
+        super().__init__()
+        self["type"] = ini_type
         if ini_type == initialisation_method.raw_values:
-            super().__init__(self.raw_values(*args, **kw_args))
-            print("\n".join(f"{repr(key)}: {repr(val)}"
-                            for key, val in self.items()))
+            self._raw_values(*args, **kw_args)
         elif ini_type == initialisation_method.diamond_default:
-            schema_tree = etree.ElementTree(kw_args.get("schema_file"))
+            self._diamond_default(kw_args["file"])
 
-    def raw_values(self, base_mesh: fd.mesh.MeshGeometry, land: Callable,
-                   sea_level: fd.Constant, times: Tuple[float, float, float],
-                   output_folder, **kw_args: dict) -> dict:
+    def _diamond_default(self, file_name):
+        tree_root = ElementTree.ElementTree(file="diamond_input.xml").getroot()
+        del tree_root[0]
+
+        self["sea_level"] = tree_root[5][0].text
+        self["mesh"] = fd.RectangleMesh(*chain(
+            zip([int(num) for num in tree_root[0][0][0].text.split(" ")],
+                [int(num) for num in tree_root[0][1][0].text.split(" ")])))
+        self["function_space"] = fd.FunctionSpace(self["mesh"], "CG", 1)
+        self["test_function"] = fd.TestFunction(self["function_space"])
+
+        self["times"] = dict(
+            zip(("current_time", "time_step", "output_time"),
+                [int(time[0].text) for time in tree_root[1]]))
+
+        # Enabled-Steps must always be the last element of the xml tree
+        self["enabled_steps"] = {
+            step.tag.lower(): bool(step.text)
+            for step in tree_root[-1]
+        }
+
+        self["out_files"] = OutputFilesCollection(tree_root[2][0].text,
+                                                  self["enabled_steps"])
+
+        land_func = tree_root[6][0].text
+        for original, replacement in CarstOptions._STRING_LIT_REPLACEMENTS:
+            land_func = land_func.replace(original, replacement)
+        land_func = "fd.project(" + land_func + ")"
+        self["land"] = eval(land_func)
+
+    def _raw_values(self, base_mesh: fd.mesh.MeshGeometry, land: Callable,
+                    sea_level: fd.Constant, times: Tuple[float, float, float],
+                    output_folder, **kw_args: dict) -> dict:
         if not isinstance(base_mesh, fd.mesh.MeshGeometry):
             raise TypeError("base_mesh not of type firedrake.Mesh")
         if not isinstance(sea_level, str):
             raise TypeError("sea_level not of type str")
 
-        vals = dict()
+        self = dict()
 
         # Store the passed values
-        vals["sea_level"] = sea_level
-        vals["times"] = dict(
+        self["sea_level"] = sea_level
+        self["times"] = dict(
             zip(("current_time", "time_step", "output_time"), times))
-        vals["mesh"] = base_mesh
+        self["mesh"] = base_mesh
 
         # Mark the steps in the process we want
-        vals["enabled_steps"] = {
+        self["enabled_steps"] = {
             "diffusion": bool(kw_args.get("diffusion")),
             "carbonates": bool(kw_args.get("carbonates")),
         }
-        vals["carbonate_production"] = kw_args.get("carbonate_production")
-        if vals["enabled_steps"]["carbonates"]:
-            if vals["carbonate_production"] is None:
+        self["carbonate_production"] = kw_args.get("carbonate_production")
+        if self["enabled_steps"]["carbonates"]:
+            if self["carbonate_production"] is None:
                 raise AttributeError(
                     "If carbonate modelling is enabled, a value for the carbonate production rate is required"
                 )
 
         # Generate our workspace from the mesh
-        vals["coordinate_space"] = fd.SpatialCoordinate(vals["mesh"])
-        vals["function_space"] = fd.FunctionSpace(vals["mesh"], "CG", 1)
-        vals["test_function"] = fd.TestFunction(vals["function_space"])
+        self["coordinate_space"] = fd.SpatialCoordinate(self["mesh"])
+        self["function_space"] = fd.FunctionSpace(self["mesh"], "CG", 1)
+        self["test_function"] = fd.TestFunction(self["function_space"])
 
         # Get our land, based in our workspace
-        vals["land"] = land(vals["coordinate_space"], vals["function_space"])
+        self["land"] = land(self["coordinate_space"], self["function_space"])
 
         # Initialise the funcs we need
-        vals["wanted_funcs"] = list(PROCESSOR_NEEDED_FUNCS["basic"])
-        for process in vals["enabled_steps"]:
-            if vals["enabled_steps"][process]:
-                vals["wanted_funcs"].extend(PROCESSOR_NEEDED_FUNCS[process])
+        self["wanted_funcs"] = list(PROCESSOR_NEEDED_FUNCS["basic"])
+        for process in self["enabled_steps"]:
+            if self["enabled_steps"][process]:
+                self["wanted_funcs"].extend(PROCESSOR_NEEDED_FUNCS[process])
 
         # Initialise out_files
-        vals["out_files"] = OutputFilesCollection(output_folder,
-                                                  vals["enabled_steps"])
-        vals["diff_coeff"] = float(kw_args.get('diff_coeff'))
-        return vals
+        self["out_files"] = OutputFilesCollection(output_folder,
+                                                  self["enabled_steps"])
+        self["diff_coeff"] = float(kw_args.get('diff_coeff'))
